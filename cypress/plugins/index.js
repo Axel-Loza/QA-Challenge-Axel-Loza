4102d0e1f0c5f2e8429e68b3a746ed196b4bb61f
Hugo Axel Loza Macias <hugo.loza@wizeline.com>
2022-05-30 17:02:22 -0500

    Add Test user on .env
    add test user as variable using .env file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,17 @@
 /// <reference types="cypress" />
+
 //add cucumber constant
 const cucumber = require('cypress-cucumber-preprocessor').default;
+require('dotenv').config();
+
+
 
 module.exports = (on, config) => {
+  config.env.VALID_EMAIL = process.env.USER_EMAIL
+  config.env.VALID_PASS = process.env.USER_PASSWORD
+  config.env.INVALID_EMAIL = process.env.INVALID_EMAIL
+  config.env.INVALID_PASS = process.env.INVALID_PASSWORD
   on('file:preprocessor', cucumber())
+  return config
 }
 
--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -2,25 +2,25 @@ class loginPage{
     
     typeValidEmail = (element) =>{
         cy.fixture('locatorsLogin.json').then((locator)=> {
-            cy.get(locator.textBoxEmailLoginPage).type("validEmail@gmail.com");
+            cy.get(locator.textBoxEmailLoginPage).type(Cypress.env('VALID_EMAIL'));
         })
     }
 
     typeValidPasswor = (element) =>{
         cy.fixture('locatorsLogin.json').then((locator)=> {
-            cy.get(locator.textBoxPasswordLoginPage).type("CorrectPassword");
+            cy.get(locator.textBoxPasswordLoginPage).type(Cypress.env('VALID_PASS'));
         })
     }
 
     typeInvalidEmail = (element) =>{
         cy.fixture('locatorsLogin.json').then((locator)=> {
-            cy.get(locator.textBoxEmailLoginPage).type("invalidEmail@gmail.com");
+            cy.get(locator.textBoxEmailLoginPage).type(Cypress.env("INVALID_EMAIL"));
         })
     }
 
     typeInvalidPasswor = (element) =>{
         cy.fixture('locatorsLogin.json').then((locator)=> {
-            cy.get(locator.textBoxPasswordLoginPage).type("invalidPassword");
+            cy.get(locator.textBoxPasswordLoginPage).type(Cypress.env("INVALID_PASS"));
         })
     }
 
