dfdf8e3b98ac00ddd2f6ba0f92f62f4c2796b23b
Hugo Axel Loza Macias <hugo.loza@wizeline.com>
2022-06-02 18:26:14 -0500

    Implement html reporter

--- src/frontend/cypress/plugins/index.js
+++ src/frontend/cypress/plugins/index.js
@@ -3,10 +3,11 @@
 // add cucumber constant
 const path = require('path')
 const cucumber = require('cypress-cucumber-preprocessor').default
-console.log('App is running on port: ', path.join(__dirname, '..', '..', '..', '..', '.env') );
 require('dotenv').config({path: path.join(__dirname, '..', '..', '..', '..', '.env')})
 
+
 module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
   config.env.VALID_EMAIL = process.env.USER_EMAIL
   config.env.VALID_PASS = process.env.USER_PASSWORD
   config.env.INVALID_EMAIL = process.env.INVALID_EMAIL
@@ -14,4 +15,3 @@ module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
   return config
 }
-// /Users/hugo.loza/Documents/QA /Axel QA challenge/QA-Challenge-Axel-Loza/.env
